af1f4659b8d06e30594d6d5d47d1c52d1fe2ccb1
albe2669 <albert@risenielsen.dk>
2020-04-22 12:55:20 +0200

    Added view models to format the output

---
+++ Classes.Webserver/Data/ViewModel/Device.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.Webserver.Data.ViewModel
+{
+    public class DeviceViewStandard
+    {
+        public long Id { get; set; }
+        public string MacAddress { get; set; }
+        public int SignalStrength { get; set; }
+    }
+}
---
+++ Classes.Webserver/Data/ViewModel/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.Webserver.Data.ViewModel
+{
+    public class RoomViewStandard
+    {
+        public long Id { get; set; }
+        public long FloorId { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public long Area { get; set; }
+    }
+}
