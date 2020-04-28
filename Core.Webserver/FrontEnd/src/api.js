1ff9b9d178e8123cb8a7f8a18d1ea57bf54f3841
albe2669 <albert@risenielsen.dk>
2020-04-28 21:24:22 +0200

    Fixed url, to workaround CORS

--- Core.Webserver/FrontEnd/src/api.js
+++ Core.Webserver/FrontEnd/src/api.js
@@ -1,7 +1,7 @@
 import exampleResponse from './api-response-max.json';
 
 export async function getData() {
-    const res = await fetch('https://127.0.0.1:44396/api/buildings/1/floors/1/rooms');
+    const res = await fetch('/api/buildings/1/floors/1/rooms');
     const data = await res.json();
 
     // Brug denne linje i stedet, hvis det gemte datasæt skal bruges
